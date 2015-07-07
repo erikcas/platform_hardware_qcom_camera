7500cb67127c2e4a00bf2e6d36b4b475087c2af8
Erik Castricum <erikcas1972@gmail.com>
2015-07-07 19:49:15 +0200

    mm-camera-interface: stick with kernel headers

--- QCamera2/stack/mm-camera-interface/Android.mk
+++ QCamera2/stack/mm-camera-interface/Android.mk
@@ -41,10 +41,10 @@ else
     LOCAL_C_INCLUDES += hardware/qcom/media/mm-core/inc
 endif
 
-ifneq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) >= 17 ))" )))
-  LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/socket.h
-  LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/un.h
-endif
+#ifneq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) >= 17 ))" )))
+#  LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/socket.h
+#  LOCAL_CFLAGS += -include bionic/libc/kernel/common/linux/un.h
+#endif
 LOCAL_CFLAGS += -Wall -Wextra -Werror
 
 LOCAL_SRC_FILES := $(MM_CAM_FILES)
